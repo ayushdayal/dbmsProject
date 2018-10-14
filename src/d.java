159263b954245f22cef22f09532dc7d86d3b024f
Siddharth <siddhartht410@gmail.com>
2018-10-14 16:48:12 +0530

    sid

--- src/d.java
+++ src/d.java
@@ -1,4 +1,8 @@
 import javax.swing.*;
+import javax.swing.event.MenuKeyEvent;
+import javax.swing.event.MenuKeyListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class d {
 
@@ -13,7 +17,63 @@ public class d {
      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
      frame.setSize(300,400);
      frame.setVisible(true);
+     JMenuBar mainmenu= new JMenuBar();
+     JMenu flight=new JMenu("FLIGHT"),staff=new JMenu("STAFF"),payment=new JMenu("PAYMENT");
+     JMenuItem reservation=new JMenuItem("CHECK RESERVATION"),update=new JMenuItem("UPDATE"),pilot=new JMenuItem("PILOT"),crew=new JMenuItem("CREW"),check =new JMenuItem("CHECK");
+/*
+     reservation.addMenuKeyListener(new MenuKeyListener() {
+            @Override
+            public void menuKeyTyped(MenuKeyEvent e) {
+                JFrame jFrame2=new JFrame();
+                jFrame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                jFrame2.setSize(300,400);
+                jFrame2.setVisible(true);
+                System.out.print("fhgf");
+            }
 
+            @Override
+            public void menuKeyPressed(MenuKeyEvent e) {
+                JFrame jFrame2=new JFrame();
+                jFrame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                jFrame2.setSize(300,400);
+                jFrame2.setVisible(true);
+                System.out.print("fhgf");
+            }
+
+            @Override
+            public void menuKeyReleased(MenuKeyEvent e) {
+
+                JFrame jFrame2=new JFrame();
+                jFrame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                jFrame2.setSize(300,400);
+                jFrame2.setVisible(true);
+                System.out.print("fhgf");
+            }
+        });
+*/
+     ActionListener actionListener =new ActionListener() {
+         @Override
+         public void actionPerformed(ActionEvent e) {
+             JFrame jFrame2=new JFrame();
+             jFrame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+             jFrame2.setSize(300,400);
+             jFrame2.setVisible(true);
+             System.out.print("fhgf");
+         }
+     };
+
+     reservation.addActionListener(actionListener);
+     flight.add(reservation);
+     flight.add(update);
+     staff.add(pilot);
+     staff.add(crew);
+     payment.add(check);
+     mainmenu.add(flight);
+     mainmenu.add(staff);
+     mainmenu.add(payment);
+
+     frame.add(mainmenu);
+     frame.setJMenuBar(mainmenu);
     }
 
     private void createUIComponents() {
