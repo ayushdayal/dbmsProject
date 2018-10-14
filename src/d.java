417cd89ce46007480ad59f5f562cfae6a47c0839
ayush <ayushdayal1998@gmail.com>
2018-10-14 13:15:01 +0530

    ayush

---
+++ src/d.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class d {
+
+    private JPanel panel1;
+    private JFrame bob;
+
+    d() {
+    }
+
+    static public void main(String args[]) {
+     JFrame frame= new JFrame("ayush");
+     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     frame.setSize(300,400);
+     frame.setVisible(true);
+
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
